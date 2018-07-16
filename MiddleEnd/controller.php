3008feef12c65710285e13bb27b7d3c413f40b0d
lmm8 <37594414+lmm8@users.noreply.github.com>
2018-07-16 19:00:31 -0400

    moved from private gitlab to public git

---
+++ MiddleEnd/controller.php
@@ -0,0 +1,34 @@
+<?php
+  $data = $_POST;
+  $call = $_POST['call'];
+  $url = array(
+    // logincheck
+    "logincheck" => "https://web.njit.edu/~lmm8/logincheck.php",
+    // grade database
+    "addgrade" => "https://web.njit.edu/~lmm8/addgrade.php",
+    "getgrade" => "https://web.njit.edu/~lmm8/getgrade.php",
+    // question database
+    "addquestion" => "https://web.njit.edu/~lmm8/addquestion.php",
+    "getquestion" => "https://web.njit.edu/~lmm8/getquestion.php",
+    // quiz database
+    "addquiz" => "https://web.njit.edu/~lmm8/addquiz.php",
+    "getquiz" => "https://web.njit.edu/~lmm8/getquiz.php",
+    // taken quiz database
+    "addtakenquiz" => "https://web.njit.edu/~lmm8/addtakenquiz.php",
+    "gettakenquiz" => "https://web.njit.edu/~lmm8/gettakenquiz.php"
+	);
+  // initialize curl
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+  curl_setopt($ch, CURLOPT_URL,$url["$call"]);
+  $output = curl_exec($ch);
+  curl_close($ch); // close the database curl
+  echo output;
+  // $output2 = json_decode($output);
+  // $response["Database"] = $output2->{"db_response"};
+  // echo json_encode($response);
+?>
---
+++ MiddleEnd/gradequiz.php
@@ -0,0 +1,25 @@
+<?php
+	$userv = explode("-", $_POST["user"]);
+	$idv = explode("-", $_POST["quizid"]);
+	// test.php
+	$test = "https://web.njit.edu/~psc26/test.php";
+	// initialize
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$test);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+	$number = count($userv);
+	// for loop
+	for($i = 0; $i < $number; $i += 1){
+		$field['user'] = $userv[$i];
+		$field['quizid'] = $idv[$i];
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $field);
+		$outputTest = curl_exec ($ch);
+		$jobjTest = json_decode($outputTest, True);
+		echo $outputTest . "<br><br>";
+	}
+	curl_close ($ch);
+	echo "Done";
+?>
